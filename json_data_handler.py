52f41d46645f4dd2d2f13a119085936511e6a18f
Morta-Ant <em.antanaviciute@gmail.com>
2023-08-06 21:07:18 +0100

    Fixed a relative import issue
    Both json_data_handler and plant-care-api need to access crud_plants.
    Because of the folder structure it couldn't be accessed correctly by both files.

--- json_data_handler.py
+++ json_data_handler.py
@@ -1,4 +1,5 @@
-import crud_plants, json
+from database.crud_plants import create_plant
+import json
 
 # plant care data
 with open("database/plant_care_data.json") as plants_care:
@@ -6,4 +7,4 @@ with open("database/plant_care_data.json") as plants_care:
 
 for plant in plant_data:
     print(f"Creating plant {plant['common_name']} with id {plant['plant_id']}...")
-    crud_plants.create_plant(plant)
+    create_plant(plant)
