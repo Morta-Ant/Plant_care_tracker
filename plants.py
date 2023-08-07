ddee20980b81d620dd0c4537b33565e23955a3ab
Efe Awatefe <efeawatefe@hotmail.com>
2023-08-07 17:34:36 +0100

    I have added users.py login.html and signup.html edited plant.py to use the login and sign up pages

--- plants.py
+++ plants.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 import json
 import requests
+from users import insert_new_record, DbConnectionError
+import re
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/plants")
 def plants():
@@ -30,13 +32,49 @@ def search():
     pass
 
 
-@app.route("/signup", methods=["POST"])
+@app.route("/signup", methods=["GET","POST"])
 def signup():
-    pass
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    success_msg = None
+    error = None
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = request.form['password']
+        password2 = request.form['password2']
+
+        if firstname == "" or len(firstname) < 3:
+            error = 'Firstname must not be blank and should contain at least 3 characters'
+        elif lastname == "" or len(lastname) < 3:
+            error = 'Lastname must not be blank and should contain at least 3 characters'
+        elif not re.fullmatch(regex, email):
+            error = 'Invalid email'
+        elif password == "" or len(password) < 6:
+            error = 'Password must not be blank and should contain at least 6 characters'
+        elif password != password2:
+            error = 'Passwords must match'
+        else:
+            
+            user = {
+                'firstname': firstname,
+                'lastname': lastname,
+                'email': email,
+                'passwd': password
+            }
+
+            try:
+                insert_new_record(user)
+                success_msg = 'You have successfully registered!'
+            except DbConnectionError:
+                error = 'Failed to register due to a database connection error'
+
+    return render_template('signup.html', error=error, msg=success_msg)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    pass
+     return render_template('login.html')
 
 @app.route("/<user>/collection")
 def user_collection():
