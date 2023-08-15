69499a738287b3baf74f9d0e405b7431c94e0ba5
Morta-Ant <em.antanaviciute@gmail.com>
2023-08-15 20:20:08 +0100

    A couple of small fixes Removed the non-existant function from import in plant-care-api Removed print(sql) from get_plants_in_user_collection() in crud_plant_collection.py which was preventing it from working

--- plant-care-api.py
+++ plant-care-api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from database.crud_plants import get_all_plants, get_plant_by_id, get_plant_by_name
-from database.crud_plant_collection import get_plant_collection_by_ids,get_plant_collection_by_user,create_plant_collection,update_plant_collection,delete_plant_collection
+from database.crud_plant_collection import get_plant_collection_by_ids,create_plant_collection,update_plant_collection,delete_plant_collection
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
