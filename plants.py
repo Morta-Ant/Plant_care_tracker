72980e1897befc19350c415861e5f3eb6bc39786
Efe Awatefe <efeawatefe@hotmail.com>
2023-08-16 22:11:49 +0100

    plant.py remove the code that uses  port 3000 reading and using only  port 5000

--- plants.py
+++ plants.py
@@ -1,32 +1,42 @@
-from flask import Flask, render_template, request
-import json, requests, re
-from database.users import insert_new_record, DbConnectionError
-from database.crud_users import create_user
+from flask import Flask, render_template, redirect, session, request, url_for,g
+import json, re, bcrypt
+from database.users import insert_new_record, DbConnectionError, get_user_by_email
+from database.crud_users import create_user,get_user_by_id
+from database.config import SECRET_KEY
+from database.crud_plants import get_all_plants,get_plant_by_id
+from database.crud_plant_collection import create_plant_collection
+from flask_login import current_user, LoginManager
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+login_manager = LoginManager
 
-# index page
 @app.route("/")
 def index():
     return render_template("home.html")
 
-# all plants page
+
 @app.route("/plants")
 def plants():
     try:
-        data = requests.get("http://127.0.0.1:3000/api/plants").json()
-        return render_template("all_plants.html", data = data)
-    except requests.exceptions.JSONDecodeError:
-        return "Oops! Something went wrong :("
-
-#pages for individual plants
+        plant_data = get_all_plants()  # Call your function to get plant data from the database
+        return render_template("all_plants.html", data=plant_data)
+    except Exception as e:
+        return f"Oops! Something went wrong: {e}"
+    
 @app.route("/plants/<int:id>")
 def one_plant(id):
     try:
-        data = requests.get(f"http://127.0.0.1:3000/api/plants/{id}").json()
-        return render_template("one_plant.html", **data)
-    except requests.exceptions.JSONDecodeError:
-        return "Oops! Something went wrong :("
+        plant_data = get_plant_by_id(id)  # Call your function to get plant data from the database
+        if plant_data:
+            return render_template("one_plant.html", **plant_data)
+        else:
+            return "Plant not found"
+    except Exception as e:
+        return f"Oops! Something went wrong: {e}"
+ 
+
+#pages for individual plants
 
 #search
 @app.route('/search', methods=['GET', 'POST'])
@@ -82,7 +92,67 @@ def signup():
 #login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-     return render_template('login.html')
+    error = None
+    success_msg = None
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        user = get_user_by_email(email)
+        # checking if passwords match
+        if user is not None:  # Check if user exists
+          is_password_correct = bcrypt.checkpw(password.encode("utf-8"), user['passwd'].encode("utf-8")) #password_db.encode("utf-8"))
+          if is_password_correct:
+             session['loggedin'] = True
+             session['id'] = user['user_id']
+             session['email'] = user['email']
+             session['firstname']=user['firstname']
+             success_msg="You are now logged in"
+             return redirect(url_for('collection'),msg=success_msg)#('home.html',msg=success_msg)
+          else:
+                error = 'Incorrect username or password, Please try agaib!'
+        else:
+            error = 'User not found!'
+
+    return render_template("login.html", error=error)
+
+#logout
+@app.route("/logout")
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.route("/collection")
+def collection():
+    
+    return render_template("collection.html")
+#########ADD to Collection
+#@app.route("/plants/<int:id>"
+
+
+@app.route("/add_to_collection", methods=["POST"])
+def add_to_collection():
+    print(session)
+    try:
+        plant_id = request.form.get("plant_id")
+        if plant_id is None:
+            return "Invalid request"
+        plant_collection = {
+            "user_id": id,  
+            "plant_id": plant_id,
+            "last_care": None,  # Set this appropriately
+            "upcoming_care": None,  # Set this appropriately
+        }
+        
+        created_collection = create_plant_collection(plant_collection)
+        if created_collection:
+            return "Plant added to collection"
+        else:
+            return "Failed to add plant to collection"
+    except Exception as e:
+        return f"Failed to add plant to collection: {e}"
+    
 
 #user collection
 @app.route("/<user>/collection")
@@ -105,7 +175,22 @@ def search_data(query):
             results.append(item)
     return results
 
-
+@app.route("/add_test_data", methods=["GET"])
+def add_test_data():
+    test_plant_collection = {
+        "user_id": 1,
+        "plant_id": 123,
+        "last_care": "2023-08-16",
+        "upcoming_care": "2023-08-23"
+    }
+    try:
+        created_collection = create_plant_collection(test_plant_collection)
+        if created_collection:
+            return "Test data added to collection"
+        else:
+            return "Failed to add test data to collection"
+    except Exception as e:
+        return f"Failed to add test data to collection: {e}"
 if __name__ == '__main__':
     app.run(debug=True)
     
