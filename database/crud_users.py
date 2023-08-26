c78b9d2cf5df05587051fe2384fcd9b6b3dbb6fa
Morta-Ant <em.antanaviciute@gmail.com>
2023-08-26 18:24:58 +0100

    Removed unused functions from crud_users

--- database/crud_users.py
+++ database/crud_users.py
@@ -1,53 +1,6 @@
 import bcrypt
 from database.database_connect import get_connector, DbConnectionError
 
-def get_all_users():
-	connector = get_connector()
-	try:
-		sql = "SELECT * FROM users"
-		cursor = connector.cursor(dictionary=True)
-		cursor.execute(sql)
-		result = cursor.fetchall()
-		cursor.close()
-		return result
-	except Exception:
-		raise DbConnectionError("Failed to read data from DB")
-	finally:
-		if connector:
-			connector.close()
-			
-def get_user_by_id(id):
-	try:
-		sql = "SELECT * FROM users WHERE user_id = %s LIMIT 1"
-		val = (id, )
-		connector = get_connector()
-		cursor = connector.cursor(dictionary=True)
-		cursor.execute(sql, val)
-		result = cursor.fetchone()
-		cursor.close()
-		return result
-	except Exception:
-		raise DbConnectionError("Failed to read data from DB, CRUD-User")
-	finally:
-		if connector:
-			connector.close()
-			
-def get_user_by_name(name):
-  try:
-    sql = "SELECT * FROM users WHERE LOWER(firstname) LIKE %s OR LOWER(lastname) LIKE %s"
-    formattedText = f"%{name.lower()}%"
-    val = (formattedText, formattedText)
-    connector = get_connector()
-    cursor = connector.cursor(dictionary=True)
-    cursor.execute(sql, val)
-    result = cursor.fetchall()
-    cursor.close()
-    return result
-  except Exception:
-    raise DbConnectionError("Failed to read data from DB")
-  finally:
-    if connector:
-      connector.close()
       
 def get_user_by_email(email):
   try:
@@ -84,39 +37,6 @@ def create_user(user):
 		if connector:
 			connector.close()
 			
-def update_user(user):
-	connector = get_connector()
-	try:
-		sql = "UPDATE users SET firstname = %s, lastname = %s, email = %s WHERE user_id = %s"
-		val = (user['firstname'], user['lastname'], user['email'], user['user_id'])
-		cursor = connector.cursor()
-		cursor.execute(sql, val)
-		connector.commit()
-		cursor.close()
-		return user
-	except Exception:
-		raise DbConnectionError("Failed to read data from DB")
-	finally:
-		if connector:
-			connector.close()
-
-def delete_user(id):
-	connector = get_connector()
-	cursor = connector.cursor()
-	try:
-		sql = "DELETE FROM users WHERE user_id = %s"
-		val = (id, )
-		cursor.execute(sql, val)
-		connector.commit()
-		cursor.close()
-		return True
-	except Exception:
-		return False
-	finally:
-		if cursor:
-			cursor.close()
-		if connector:
-			connector.close()
 
 def get_user_emails():
 	connector = get_connector()
@@ -134,6 +54,3 @@ def get_user_emails():
 	finally:
 		if connector:
 			connector.close()
-
-
-
